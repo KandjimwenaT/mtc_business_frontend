9e22e0586cbe12ed91cc7acb0a7d187000bb9904
Tangi Kandjimwena <tkandjimwena@mtc.com.na>
2026-04-08 09:41:51 +0200

    UAT

--- src/app/api/apiBase.ts
+++ src/app/api/apiBase.ts
@@ -2,4 +2,6 @@
 //   import.meta.env.VITE_API_BASE_URL || "http://41.219.71.27:3000/api";
 
 
-export const API_BASE_URL = "http://localhost:3000/api";
+// export const API_BASE_URL = "http://localhost:3000/api";
+
+export const API_BASE_URL = "http://uat-api.erongored.com.na/mtc/";
